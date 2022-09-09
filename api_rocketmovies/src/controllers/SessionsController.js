6e98a6b13e91b46223efce42ce2321dc66459ef6
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-09 09:07:26 -0300

    Validação de senha

--- api_rocketmovies/src/controllers/SessionsController.js
+++ api_rocketmovies/src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionsController {
   async create(request, response) {
@@ -7,8 +8,14 @@ class SessionsController {
 
     const user = await knex("users").where({ email }).first();
 
-    if(!user){
-        throw new AppError("Email e/ou senha incorreta",401)
+    if (!user) {
+      throw new AppError("Email e/ou senha incorreta", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Email e/ou senha incorreta", 401);
     }
 
     return response.json(user);
