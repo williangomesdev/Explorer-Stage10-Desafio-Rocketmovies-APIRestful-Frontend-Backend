269691cf60cea396fcc4353c48d89e1c6386eced
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-10 17:28:14 -0300

    Carregando Imagens

--- api_rocketmovies/src/routes/user.routes.js
+++ api_rocketmovies/src/routes/user.routes.js
@@ -1,12 +1,24 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.createUser);
 usersRoutes.put("/", ensureAuthenticated, usersController.updateUser);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  (req, res) => {
+    console.log(req.file.filename);
+    res.json();
+  }
+);
 
 module.exports = usersRoutes;
