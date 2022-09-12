bd17ea1e4358c118495c8c314b3b4c32b897f7a0
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-12 12:55:34 -0300

    SignIn

--- rocketmovies/src/hooks/auth.jsx
+++ rocketmovies/src/hooks/auth.jsx
@@ -1,12 +1,27 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { api } from "../../../api_rocketmovies/src/services/api";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({});
+  
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+      const { user, token } = response.data;
+      api.defaults.headers.authorization = `Bearer ${token}`;
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.message);
+      } else {
+        alert("Não foi possível entrar");
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ name: "", email: "" }}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
   );
 }
 
--- rocketmovies/src/pages/SignIn/index.jsx
+++ rocketmovies/src/pages/SignIn/index.jsx
@@ -5,9 +5,17 @@ import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
 import { Container, Form, Background } from "./styles";
 import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export function SignIn() {
-  const data = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
 
   return (
     <Container>
@@ -15,9 +23,19 @@ export function SignIn() {
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assiste</p>
         <h2>Faça seu login</h2>
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Button title="Entrar" />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <Button title="Entrar" onClick={handleSignIn} />
         <Link to="/register">Criar conta</Link>
       </Form>
       <Background />
