5a17963d92528936c01fa89924ccf086a085b7c8
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-12 12:32:45 -0300

    hook de autenticação

--- rocketmovies/src/pages/SignIn/index.jsx
+++ rocketmovies/src/pages/SignIn/index.jsx
@@ -1,24 +1,26 @@
-import { FiMail, FiLock } from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
-import { Button } from '../../components/Button'
-import { ButtonText } from '../../components/ButtonText'
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 import { Container, Form, Background } from "./styles";
+import { useAuth } from "../../hooks/auth";
 
 export function SignIn() {
-    return (
-        <Container>
-            <Form>
-                <h1>RocketMovies</h1>
-                <p>Aplicação para acompanhar tudo que assiste</p>
-                <h2>Faça seu login</h2>
-                <Input placeholder="E-mail" type="text" icon={FiMail} />
-                <Input placeholder="Senha" type="password" icon={FiLock} />
-                <Button title="Entrar" />
-                <Link to="/register">Criar conta</Link>
+  const data = useAuth();
 
-            </Form>
-            <Background />
-        </Container>
-    )
+  return (
+    <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assiste</p>
+        <h2>Faça seu login</h2>
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Button title="Entrar" />
+        <Link to="/register">Criar conta</Link>
+      </Form>
+      <Background />
+    </Container>
+  );
 }
