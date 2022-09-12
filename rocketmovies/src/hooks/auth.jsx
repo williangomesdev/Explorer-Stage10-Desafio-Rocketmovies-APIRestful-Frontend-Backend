324015e33928a64e0a4d7d18b9088ecea4eda756
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-12 13:20:21 -0300

    Redirecionamento de rotas

--- rocketmovies/src/hooks/auth.jsx
+++ rocketmovies/src/hooks/auth.jsx
@@ -5,12 +5,13 @@ export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [data, setData] = useState({});
-  
+
   async function signIn({ email, password }) {
     try {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
       api.defaults.headers.authorization = `Bearer ${token}`;
+      setData({ user, token });
     } catch (error) {
       if (error.response) {
         alert(error.response.message);
@@ -21,7 +22,9 @@ function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
