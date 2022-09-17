ed1dbdf0282ddcdb0d3481219990c4f320a63dc3
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-17 11:54:41 -0300

    Removendo tags

--- rocketmovies/src/pages/CreateMovie/index.jsx
+++ rocketmovies/src/pages/CreateMovie/index.jsx
@@ -16,6 +16,10 @@ export function CreateMovie() {
     setNewTag("");
   }
 
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -40,7 +44,9 @@ export function CreateMovie() {
               <CreateMovieTag
                 key={String(index)}
                 value={tag}
-                onClick={() => {}}
+                onClick={() => {
+                  handleRemoveTag(tag);
+                }}
               />
             ))}
             <CreateMovieTag
