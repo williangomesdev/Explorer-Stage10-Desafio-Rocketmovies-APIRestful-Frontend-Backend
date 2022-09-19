cd0faaa20da5be12ef391e9c30c5d8c6353deb8b
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-19 11:05:14 -0300

    Cadastrar nota

--- rocketmovies/src/pages/CreateMovie/index.jsx
+++ rocketmovies/src/pages/CreateMovie/index.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { FiArrowLeft } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { CreateMovieTag } from "../../components/CreateMovieTag";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Container, Form } from "./styles";
 
+import { api } from "../../../../api_rocketmovies/src/services/api";
+
 export function CreateMovie() {
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+  const [description, setDescription] = useState("");
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
 
   function handleAddTag() {
     setTags((prevState) => [...prevState, newTag]);
@@ -18,6 +25,17 @@ export function CreateMovie() {
 
   function handleRemoveTag(deleted) {
     setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+  }
+
+  async function handleNewMovieNote() {
+    await api.post("/notes", {
+      title,
+      description,
+      rating,
+      tags,
+    });
+    alert("Nota criada com sucesso!");
+    navigate("/");
   }
 
   return (
@@ -34,10 +52,21 @@ export function CreateMovie() {
           </header>
 
           <div className="titleInput">
-            <Input placeholder="Título" type="text" />
-            <Input placeholder="Sua nota (de 0 a 5)" type="text" />
+            <Input
+              placeholder="Título"
+              type="text"
+              onChange={(event) => setTitle(event.target.value)}
+            />
+            <Input
+              placeholder="Sua nota (de 0 a 5)"
+              type="text"
+              onChange={(event) => setRating(event.target.value)}
+            />
           </div>
-          <textarea placeholder="Observações" />
+          <textarea
+            placeholder="Observações"
+            onChange={(event) => setDescription(event.target.value)}
+          />
           <h2>Marcadores</h2>
           <div className="tagSpace">
             {tags.map((tag, index) => (
@@ -60,7 +89,7 @@ export function CreateMovie() {
 
           <div>
             <Button title="Excluir filme" />
-            <Button title="Salvar filme" />
+            <Button title="Salvar filme" onClick={handleNewMovieNote} />
           </div>
         </Form>
       </main>
